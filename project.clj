cef1df10265a3cf1fb797cb77da60520c8175600
Sean Shillo <revshillo@yahoo.com>
2013-04-29 21:30:20 -0400

    tutorial 2

---
+++ project.clj
@@ -0,0 +1,15 @@
+(defproject modern-cljs "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :source-paths ["src/clj"]
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :plugins [[lein-cljsbuild "0.3.0"]]
+  :cljsbuild {:builds
+              [{
+                :source-paths ["src/cljs"]
+                :compiler {
+                           :output-to "resources/public/js/modern.js"
+                           :optimizations :whitespace
+                           :pretty-print true}}]})
